2574553d222c1995873bb60f9a2b6002a4649a34
adam syafi <19ftt1188@student.pb.edu.bn>
2022-04-16 09:54:51 +0800

    Successfully installed tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div class="bg-green-900 w-1/8 p-10 h-auto rounded-lg">
+<img src="./assets/as3_picture.jpg" class="object-contain h-48 w-96 rounded-lg" alt="">
+<p> Adam Syafi 19FTT1188</p>
+</div>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
